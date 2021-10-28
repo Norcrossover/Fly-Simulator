6bd13922754f8c5c1f42b6327526f62f5397eb15
Kobe <kobenorcross@gmail.com>
2021-10-28 17:44:13 -0600

    Importing new diffeqpy library to use

---
+++ fly.py
@@ -0,0 +1,13 @@
+# constants 
+flies = 100
+mew_one = 0.16
+mew_two = 0.21
+mew_three = 0.08
+mew_four = 0.16
+capital_alpha= 10**-6
+epsilon_beta_gamma = 0.0016
+delta = 1.14226
+lower_alpha = 0.003 * 10**-6
+
+class Fly(object):
+    def __init__(self, age, )
