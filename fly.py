a7b420b00fe521c37f26f9343b41da7f22990270
Kobe <kobenorcross@gmail.com>
2021-11-08 14:11:02 -0700

    Learning how to use matplotlib

--- fly.py
+++ fly.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 # constants 
+#plt.use('fast')
 flies = 100
 tspan = 10
 mew_one = 0.16
@@ -14,14 +15,24 @@ lower_alpha = 0.003 * 10**-6
 
 # Creation of a rate to be used in the exponential
 def rate_of_mortality():
-    return nothing
+    return 0
 
 # will recursively return the number of flies
 def number_of_flies():
-    return nothing
+    return 0
 
 def graph_data():
-    return nothing
+    fig, ax = plt.subplots()
+    plt.draw()
+    plt.ion()
+    plt.title("Fly Simulation Test")
+    plt.xlabel("Time")
+    plt.ylabel("Number of Flies")
+    ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
+    plt.show()
 
 def main():
     graph_data()
+
+if __name__ == "__main__":
+    main()
