6e9cedc209514cb362cf07ae554c88ca0c084b07
Kobe <kobenorcross@gmail.com>
2021-11-08 15:06:32 -0700

    testing how to make an array out of the recursive function

--- fly.py
+++ fly.py
@@ -12,27 +12,33 @@ capital_alpha= 10**-6
 epsilon_beta_gamma = 0.0016
 delta = 1.14226
 lower_alpha = 0.003 * 10**-6
+months = 12
 
 # Creation of a rate to be used in the exponential
 def rate_of_mortality():
-    return 0
+    return 1.2
 
 # will recursively return the number of flies
-def number_of_flies():
-    return 0
+def number_of_flies(months):
+    if (months != 0): 
+        return (rate_of_mortality * number_of_flies(months-1))
+    #return number_of_flies() + (rate_of_mortality * (K - number_of_flies()) / K)
 
-def graph_data():
-    fig, ax = plt.subplots()
-    plt.draw()
-    plt.ion()
+def graph_data(months):
+    x = []
+    x.append(number_of_flies(months, x))
+    
+    print(x)
+    y = [0, months]
     plt.title("Fly Simulation Test")
     plt.xlabel("Time")
     plt.ylabel("Number of Flies")
-    ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
+    plt.plot(x, y)
     plt.show()
 
+
 def main():
-    graph_data()
+    graph_data(months)
 
 if __name__ == "__main__":
     main()
