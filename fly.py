898f3918c50cca506ade085109cca61d1ce0b085
Kobe <kobenorcross@gmail.com>
2021-11-08 15:37:37 -0700

    finished basic application, now time to implement actual equations

--- fly.py
+++ fly.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-# constants 
-#plt.use('fast')
+# constants/globals 
 flies = 100
 tspan = 10
 mew_one = 0.16
@@ -13,27 +12,43 @@ epsilon_beta_gamma = 0.0016
 delta = 1.14226
 lower_alpha = 0.003 * 10**-6
 months = 12
+initial_flies = 3
+x = []
+y = []
 
 # Creation of a rate to be used in the exponential
 def rate_of_mortality():
-    return 1.2
+    return 120
 
 # will recursively return the number of flies
 def number_of_flies(months):
-    if (months != 0): 
-        return (rate_of_mortality * number_of_flies(months-1))
+    ''' Recursive Solution, can't figure it out
+    if (months == 0): 
+        return initial_flies
+    #x.append(rate_of_mortality * flies)
+    return (rate_of_mortality * number_of_flies(months-1))    
     #return number_of_flies() + (rate_of_mortality * (K - number_of_flies()) / K)
+    '''
+    flies = initial_flies
+    x.append(initial_flies)
+    y.append(0)
+    for i in range(months):
+        flies = (rate_of_mortality() * flies)
+        x.append(flies)
+        y.append(i)
+
 
 def graph_data(months):
-    x = []
-    x.append(number_of_flies(months, x))
-    
+    number_of_flies(months)
     print(x)
-    y = [0, months]
+    print(y)
+    # change them to numpy arrays 
+    x_np = np.array(x)
+    y_np = np.array(y)
     plt.title("Fly Simulation Test")
     plt.xlabel("Time")
     plt.ylabel("Number of Flies")
-    plt.plot(x, y)
+    plt.plot(x_np, y_np)
     plt.show()
 
 
