43d92e124fd3ed9770a74b322fe847ce5aad13b8
Kobe <kobenorcross@gmail.com>
2021-11-07 22:20:11 -0700

    Back to square one with an insane push in the design in the right direction :)

--- fly.py
+++ fly.py
@@ -1,5 +1,6 @@
 # constants 
 flies = 100
+tspan = 10
 mew_one = 0.16
 mew_two = 0.21
 mew_three = 0.08
@@ -9,5 +10,3 @@ epsilon_beta_gamma = 0.0016
 delta = 1.14226
 lower_alpha = 0.003 * 10**-6
 
-class Fly(object):
-    def __init__(self, age, )
